f2d69a3b96ee1f45ebbd82457ab766440a7b1cff
Walterlayzz <93656199+Walterlayzz@users.noreply.github.com>
2022-01-25 10:39:59 +0800

    Add files via upload

---
+++ Question2a.java
@@ -0,0 +1,6 @@
+public class Question2a {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, I am Walter Lay!");
+    }
+}
---
+++ Question2c.java
@@ -0,0 +1,13 @@
+public class Question2c {
+    public static void main (String[] args)
+    {
+        for (int x = 102; x >= 66; x = x-1)
+        {
+            if (x%2 != 0)
+            {
+                System.out.print("Value of x : " + x);
+                System.out.print("\n");
+            }
+        }
+    }
+}
